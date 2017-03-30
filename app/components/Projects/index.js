298bb2b47502e01573f4e106ce24fd4c326add3f
Valery Bugakov <skymk1@gmail.com>
2017-03-30 23:06:36 +0400

    Completed project list

--- app/components/Projects/index.js
+++ app/components/Projects/index.js
@@ -4,7 +4,7 @@ import Relay from 'react-relay'
 import R from 'ramda'
 import Project from './Project'
 
-export const Projects = ({ viewer: { repositories, actor } = {} }) => (
+export const Projects = ({ viewer: { repositories } }) => (
   <div>
     <Helmet
       title="Projects"
@@ -12,10 +12,6 @@ export const Projects = ({ viewer: { repositories, actor } = {} }) => (
         { name: 'description', content: 'Description of Projects' },
       ]}
     />
-    {
-      actor &&
-      <div>Viewer: {actor.userName}</div>
-    }
     {
       R.pipe(
         R.path(['edges']),
@@ -37,9 +33,6 @@ export default Relay.createContainer(Projects, {
   fragments: {
     viewer: () => Relay.QL`
       fragment on Viewer {
-        actor {
-          userName
-        }
         repositories(first: 10) {
           edges {
             node {
---
+++ app/components/shared/repo/TitleLink.js
@@ -0,0 +1,38 @@
+import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
+import styled from 'styled-components'
+import { Glyphicon } from 'react-bootstrap';
+
+const Container = styled.span`
+  font-size: 16px;
+  margin-right: 15px;
+`
+const Separator = styled.span`
+  margin: 0 3px;
+`
+const AccessIcon = styled(Glyphicon)`
+  display: inline-block;
+  margin-left: 10px;
+  opacity: 0.6;
+  font-size: 14px;
+`
+
+const TitleLink = ({ userName, repoName, isPrivate }) => (
+  <Container>
+    <Link to={`profile/${userName}`}>{userName}</Link>
+    <Separator>/</Separator>
+    <Link to={`/${userName}/${repoName}`}>{repoName}</Link>
+    {
+      typeof isPrivate !== 'undefined' &&
+      <AccessIcon glyph={isPrivate ? 'flash' : 'lock'} />
+    }
+  </Container>
+)
+
+TitleLink.propTypes = {
+  isPrivate: PropTypes.bool,
+  userName: PropTypes.string.isRequired,
+  repoName: PropTypes.string.isRequired,
+}
+
+export default TitleLink
